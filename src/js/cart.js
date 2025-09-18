7621f3a5cc35aff4184c2ea536c50c5c640d7c99
Temple Ohanu <emperorvikkie@gmail.com>
2025-09-19 00:05:48 +0100

    Added total price function

--- src/js/cart.js
+++ src/js/cart.js
@@ -4,6 +4,21 @@ function renderCartContents() {
   const cartItems = getLocalStorage("so-cart");
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
+
+  const cartFooter = document.querySelector(".cart-footer");
+  const cartTotal = document.querySelector(".cart-total");
+
+  if (cartItems && cartItems.length > 0) {
+    // Calculate total
+    const total = cartItems.reduce(
+      (sum, item) => sum + Number(item.FinalPrice || item.ListPrice || 0),
+      0,
+    );
+    cartFooter.classList.remove("hide");
+    cartTotal.textContent = `Total: $${total.toFixed(2)}`;
+  } else {
+    cartFooter.classList.add("hide");
+  }
 }
 
 function cartItemTemplate(item) {
